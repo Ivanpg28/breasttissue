a81494c7959a4cb9e50f5638d07da0b172e308f5
Iván Pérez <ivanpg28@gmail.com>
2020-03-23 21:08:59 +0100

    funcionando conexión

--- esclavo.c
+++ esclavo.c
@@ -31,17 +31,26 @@ struct Calculo
 	float p;
 };
 
+void logger(char *texto)
+{
+	FILE *fichero = NULL;
+	fichero = fopen("pvm3/bin/LINUX/esclavo.log", "a");
+	fprintf(fichero, "%s\n", texto);
+	fclose(fichero);
+}
+
 struct Caso *leerCasos(char *path)
 {
+	logger("entra leer");
 	FILE *fichero = NULL;
 	int dim = 100;
 	int i = 0;
 	struct Caso *casos = malloc(dim * sizeof(struct Caso));
-
-    for (int j = 0; j < dim; j++)
-    {
-        casos[j].id = 0;
-    }
+	int j;
+	for (j = 0; j < dim; j++)
+	{
+		casos[j].id = 0;
+	}
 
 	fichero = fopen(path, "r");
 	if (!fichero == NULL)
@@ -66,15 +75,15 @@ struct Caso *leerCasos(char *path)
 
 struct Caso normalizarCaso(struct Caso caso, struct Calculo media, struct Calculo dest)
 {
-	caso.i0 = (caso.i0 - media.i0)/ dest.i0;
-	caso.pa500 = (caso.pa500 - media.pa500)/ dest.pa500;
-	caso.hfs = (caso.hfs - media.hfs)/ dest.hfs;
-	caso.da = (caso.da - media.da)/ dest.da;
-	caso.area = (caso.area - media.area)/ dest.area;
-	caso.ada = (caso.ada - media.ada)/ dest.ada;
-	caso.max_ip = (caso.max_ip - media.max_ip)/ dest.max_ip;
-	caso.dr = (caso.dr - media.dr)/ dest.dr;
-	caso.p = (caso.p - media.p)/ dest.p;
+	caso.i0 = (caso.i0 - media.i0) / dest.i0;
+	caso.pa500 = (caso.pa500 - media.pa500) / dest.pa500;
+	caso.hfs = (caso.hfs - media.hfs) / dest.hfs;
+	caso.da = (caso.da - media.da) / dest.da;
+	caso.area = (caso.area - media.area) / dest.area;
+	caso.ada = (caso.ada - media.ada) / dest.ada;
+	caso.max_ip = (caso.max_ip - media.max_ip) / dest.max_ip;
+	caso.dr = (caso.dr - media.dr) / dest.dr;
+	caso.p = (caso.p - media.p) / dest.p;
 
 	return caso;
 }
@@ -83,7 +92,8 @@ struct Caso *normalizarCasos(struct Caso *casos, struct Calculo media, struct Ca
 {
 	int i = 0;
 
-	while(casos[i].id != 0) {
+	while (casos[i].id != 0)
+	{
 		casos[i] = normalizarCaso(casos[i], media, dest);
 		i++;
 	}
@@ -95,57 +105,59 @@ void escribirCasos(struct Caso *casos, char *path)
 {
 	FILE *fichero = NULL;
 	int i = 0;
-
 	fichero = fopen(path, "w");
 
-	while(casos[i].id != 0) {
+	while (casos[i].id != 0)
+	{
 		fprintf(fichero, "\n%d,%s,%f,%f,%f,%f,%f,%f,%f,%f,%f", casos[i].id, casos[i].class, casos[i].i0, casos[i].pa500, casos[i].hfs, casos[i].da, casos[i].area, casos[i].ada, casos[i].max_ip, casos[i].dr, casos[i].p);
 		i++;
 	}
 	fclose(fichero);
 }
 
-main() {
-	int ptid,nbytes,msgtag,tid;
+main()
+{
+	logger("inicia");
+	int ptid, nbytes, msgtag, tid;
 	int n1, n2, rsdo = 1, i, j = 0;
-    struct Calculo media;
-    struct Calculo dest;
+	struct Calculo media;
+	struct Calculo dest;
 	struct Caso *casos;
 	struct Caso *casos_normalizados;
-	char *fichero;
-	char *fichero_destino;
-   	char *b;
-	
-	
-	ptid=pvm_parent();
-	int bufid=pvm_recv(ptid,-1);
-	pvm_bufinfo(bufid,&nbytes,&msgtag,&tid);//msgtag será 1 o 2 dependiendo de lo que envíe el maestro
-	pvm_upkbyte(b,1,0); //desempaqueto 1 dato desde la posicion 0
-	//pvm_upkint(&n2,1,0);
+	char path[50] = "pvm3/bin/LINUX/";
+	char fichero[50];
+	char fichero_destino[50] = "pvm3/bin/LINUX/BreastTissueTrainN.csv";
+	char b[sizeof(media)], c[sizeof(dest)];
+
+	ptid = pvm_parent();
+	logger("conecta padre");
+	int bufid = pvm_recv(ptid, -1);
+	pvm_bufinfo(bufid, &nbytes, &msgtag, &tid); //msgtag será 1 o 2 dependiendo de lo que envíe el maestro
+	pvm_upkstr(fichero);
+	logger("desempaqueta fichero");
+	pvm_upkbyte(b, sizeof(media), 0);
+	logger("desempaqueta media");
 	memcpy(&media, b, sizeof(media));
-	printf("\nMedia: %f,%f,%f,%f,%f,%f,%f,%f,%f", media.i0, media.pa500, media.hfs, media.da, media.area, media.ada, media.max_ip, media.dr, media.p);
-	if(msgtag == 1){
-		for (i = 0; i < strlen(fichero); i++){
-			if(fichero[i] == '.'){
-				fichero_destino[j] = 'N';
-				j++;
-			}
-			fichero_destino[j] = fichero[i];
-			j++;
-		}
-		casos = leerCasos(fichero);
+	pvm_upkbyte(c, sizeof(dest), 0);
+	logger("desempaqueta dest");
+	memcpy(&dest, c, sizeof(dest));
+
+	if (msgtag == 1)
+	{
+		casos = leerCasos("pvm3/bin/LINUX/BreastTissueTrain.csv");
+		logger("sale leer");
 		casos_normalizados = normalizarCasos(casos, media, dest);
-		escribirCasos(casos_normalizados, fichero_destino);
+		logger("normaliza datos");
+		escribirCasos(casos, fichero_destino);
+		logger("escribe datos");
 	}
-	else if (msgtag==2){
+	else if (msgtag == 2)
+	{
 		//Parte 2
 	}
 	pvm_initsend(PvmDataDefault); /* inicializar el buffer */
-	//pvm_pkint(&rsdo,1,0);  /* empaqueta el resultado y lo envía al padre*/
-	pvm_pkint(&rsdo,1,0);  /* empaqueta el resultado y lo envía al padre*/
-	pvm_send(ptid,0); 
+	pvm_pkint(&rsdo, 1, 0);		  /* empaqueta el resultado y lo envía al padre*/
+	pvm_send(ptid, 0);
 	pvm_exit();
-	free(casos);
-	free(casos_normalizados);
-	exit(0); 
+	exit(0);
 }
--- maestro.c
+++ maestro.c
@@ -32,13 +32,15 @@ struct Calculo
 	float p;
 };
 
-struct Calculo calcularMedia(struct Caso *casos) {
-    struct Calculo media;
-    float sum = 0.0;
-    int tam;
-    int i = 0;
-    while(casos[i].id != 0) {
-        media.i0 += casos[i].i0;
+struct Calculo calcularMedia(struct Caso *casos)
+{
+	struct Calculo media;
+	float sum = 0.0;
+	int tam;
+	int i = 0;
+	while (casos[i].id != 0)
+	{
+		media.i0 += casos[i].i0;
 		media.pa500 += casos[i].pa500;
 		media.hfs += casos[i].hfs;
 		media.da += casos[i].da;
@@ -48,7 +50,7 @@ struct Calculo calcularMedia(struct Caso *casos) {
 		media.dr += casos[i].dr;
 		media.p += casos[i].p;
 		i++;
-    }
+	}
 	media.i0 /= i;
 	media.pa500 /= i;
 	media.hfs /= i;
@@ -59,15 +61,17 @@ struct Calculo calcularMedia(struct Caso *casos) {
 	media.dr /= i;
 	media.p /= i;
 
-    return media;
+	return media;
 }
 
-struct Calculo calcularDest(struct Caso *casos, struct Calculo media) {
-    struct Calculo dest;
-    int i = 0;
+struct Calculo calcularDest(struct Caso *casos, struct Calculo media)
+{
+	struct Calculo dest;
+	int i = 0;
 
-    while(casos[i].id != 0) {
-        dest.i0 += pow(casos[i].i0 - media.i0, 2);
+	while (casos[i].id != 0)
+	{
+		dest.i0 += pow(casos[i].i0 - media.i0, 2);
 		dest.pa500 += pow(casos[i].pa500 - media.pa500, 2);
 		dest.hfs += pow(casos[i].hfs - media.hfs, 2);
 		dest.da += pow(casos[i].da - media.da, 2);
@@ -77,7 +81,7 @@ struct Calculo calcularDest(struct Caso *casos, struct Calculo media) {
 		dest.dr += pow(casos[i].dr - media.dr, 2);
 		dest.p += pow(casos[i].p - media.p, 2);
 		i++;
-    }
+	}
 
 	dest.i0 = sqrt(dest.i0 / i);
 	dest.pa500 = sqrt(dest.pa500 / i);
@@ -89,7 +93,7 @@ struct Calculo calcularDest(struct Caso *casos, struct Calculo media) {
 	dest.dr = sqrt(dest.dr / i);
 	dest.p = sqrt(dest.p / i);
 
-	 return dest;
+	return dest;
 }
 
 struct Caso *leerCasos(char *path)
@@ -98,11 +102,11 @@ struct Caso *leerCasos(char *path)
 	int dim = 100;
 	int i = 0;
 	struct Caso *casos = malloc(dim * sizeof(struct Caso));
-
-    for (int j = 0; j < dim; j++)
-    {
-        casos[j].id = 0;
-    }
+	int j;
+	for (j = 0; j < dim; j++)
+	{
+		casos[j].id = 0;
+	}
 
 	fichero = fopen(path, "r");
 	if (!fichero == NULL)
@@ -127,46 +131,44 @@ struct Caso *leerCasos(char *path)
 
 void mostrarCasos(struct Caso *casos)
 {
-    int i = 0;
-    while (casos[i].id != 0)
-    {
-        printf("\n%d,%s,%f,%f,%f,%f,%f,%f,%f,%f,%f", casos[i].id, casos[i].class, casos[i].i0, casos[i].pa500, casos[i].hfs, casos[i].da, casos[i].area, casos[i].ada, casos[i].max_ip, casos[i].dr, casos[i].p);
-        i++;
-    }
-    printf("\n");
+	int i = 0;
+	while (casos[i].id != 0)
+	{
+		printf("\n%d,%s,%f,%f,%f,%f,%f,%f,%f,%f,%f", casos[i].id, casos[i].class, casos[i].i0, casos[i].pa500, casos[i].hfs, casos[i].da, casos[i].area, casos[i].ada, casos[i].max_ip, casos[i].dr, casos[i].p);
+		i++;
+	}
+	printf("\n");
 }
 
 main()
 {
 	int cc1, cc2, tid;
-	int tarea = 1; //tarea = 1 suma, tarea = 2 resta
+	int tarea = 1;
 	int n1, n2, rsdo;
-	char *fichero = "BreastTissueTrain.csv";
+	char fichero[50] = "BreastTissueTrain.csv";
 
 	struct Caso *casos = leerCasos(fichero);
 	mostrarCasos(casos);
 
-   	struct Calculo media = calcularMedia(casos);
-   	printf("\nMedia: %f,%f,%f,%f,%f,%f,%f,%f,%f", media.i0, media.pa500, media.hfs, media.da, media.area, media.ada, media.max_ip, media.dr, media.p);
-	
+	struct Calculo media = calcularMedia(casos);
+	printf("\nMedia: %f,%f,%f,%f,%f,%f,%f,%f,%f", media.i0, media.pa500, media.hfs, media.da, media.area, media.ada, media.max_ip, media.dr, media.p);
+
 	struct Calculo dest = calcularDest(casos, media);
 	printf("\nDesviacion Estandar: %f,%f,%f,%f,%f,%f,%f,%f,%f", dest.i0, dest.pa500, dest.hfs, dest.da, dest.area, dest.ada, dest.max_ip, dest.dr, dest.p);
 
-   
 	printf("El id del maestro es %x\n", pvm_mytid()); /* enrolar en la PVM */
 	/* arrancar 1 copia del proceso esclavo en otra maquina */
-	cc1 = pvm_spawn("esclavo", NULL, 1, "ubuntu-nodo-1", 1, &tid);
+	cc1 = pvm_spawn("esclavo", NULL, 1, "ubuntu-nodo1", 1, &tid);
 	if (cc1 == 1)
 	{
 		pvm_initsend(PvmDataDefault); /* inicializar el buffer */
 		char media_ser[sizeof(media)];
-    	memcpy(media_ser, &media, sizeof(media));
 		char dest_ser[sizeof(dest)];
-    	memcpy(dest_ser, &dest, sizeof(dest));
+		memcpy(media_ser, &media, sizeof(media));
+		memcpy(dest_ser, &dest, sizeof(dest));
 		pvm_pkstr(fichero);
-		pvm_pkbyte(media_ser, 1, 0);		  /* empaquetar datos y enviar al esclavo*/
-		pvm_pkbyte(dest_ser, 1, 0);		  /* empaquetar datos y enviar al esclavo*/
-		//pvm_pkint(&n2, 1, 0);
+		pvm_pkbyte(media_ser, sizeof(media_ser), 1);
+		pvm_pkbyte(dest_ser, sizeof(dest_ser), 0);
 		pvm_send(tid, tarea);   /* tarea indica al esclavo si debe sumar o restar */
 		cc2 = pvm_recv(-1, -1); /* recibir el resultado de la operación realizada en el esclavo */
 		pvm_bufinfo(cc2, (int *)0, (int *)0, &tid);
